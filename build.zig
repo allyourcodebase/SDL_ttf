86eb71e41da14a0ddd528119ae5ade9eaff11a62
Jay Petacat <jay@jayschwa.net>
2025-12-24 16:12:42 -0700

    Fix build for Zig 0.16.0-dev.1657+985a3565c, drop 0.14.x support

--- build.zig
+++ build.zig
@@ -9,41 +9,43 @@ pub fn build(b: *std.Build) void {
 
     const upstream = b.dependency("SDL_ttf", .{});
 
+    const mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .sanitize_c = sanitize_c,
+    });
+
     const lib = b.addLibrary(.{
         .name = "SDL2_ttf",
         .version = .{ .major = 2, .minor = 24, .patch = 0 },
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-            .sanitize_c = sanitize_c,
-        }),
+        .root_module = mod,
     });
-    lib.addCSourceFile(.{ .file = upstream.path("SDL_ttf.c") });
+    mod.addCSourceFile(.{ .file = upstream.path("SDL_ttf.c") });
 
     if (harfbuzz_enabled) {
         const harfbuzz_dep = b.dependency("harfbuzz", .{
             .target = target,
             .optimize = optimize,
         });
-        lib.linkLibrary(harfbuzz_dep.artifact("harfbuzz"));
-        lib.root_module.addCMacro("TTF_USE_HARFBUZZ", "1");
+        mod.linkLibrary(harfbuzz_dep.artifact("harfbuzz"));
+        mod.addCMacro("TTF_USE_HARFBUZZ", "1");
     }
 
     const freetype_dep = b.dependency("freetype", .{
         .target = target,
         .optimize = optimize,
     });
-    lib.linkLibrary(freetype_dep.artifact("freetype"));
+    mod.linkLibrary(freetype_dep.artifact("freetype"));
 
     const sdl_dep = b.dependency("SDL", .{
         .target = target,
         .optimize = optimize,
     });
     const sdl_lib = sdl_dep.artifact("SDL2");
-    lib.linkLibrary(sdl_lib);
-    lib.addIncludePath(sdl_lib.getEmittedIncludeTree().path(b, "SDL2"));
+    mod.linkLibrary(sdl_lib);
+    mod.addIncludePath(sdl_lib.getEmittedIncludeTree().path(b, "SDL2"));
 
     lib.installHeader(upstream.path("SDL_ttf.h"), "SDL2/SDL_ttf.h");
 
